a1a924a5d6c9a46caf1e2e3ffa5c8f4daf1e2de9
Elena Sinelnikova <elle.sinelle@gmail.com>
2026-02-13 17:51:54 +0300

    fix: fix peer connection, add iceServeres

--- app/lib/peer.ts
+++ app/lib/peer.ts
@@ -15,7 +15,23 @@ class PeerService {
 
   initialize(): Promise<string> {
     return new Promise((resolve, reject) => {
-      this.peer = new Peer();
+      this.peer = new Peer({
+        config: {
+          iceServers: [
+            { urls: 'stun:stun.l.google.com:19302' },
+            { urls: 'stun:stun1.l.google.com:19302' },
+            { urls: 'stun:stun2.l.google.com:19302' },
+            { urls: 'stun:stun3.l.google.com:19302' },
+            { urls: 'stun:stun4.l.google.com:19302' },
+            { urls: 'stun:stun.ekiga.net' },
+            { urls: 'stun:stun.ideasip.com' },
+            { urls: 'stun:stun.schlund.de' },
+            { urls: 'stun:stun.stunprotocol.org:3478' },
+            { urls: 'stun:stun.voiparound.com' },
+            { urls: 'stun:stun.voipbuster.com' },
+          ],
+        },
+      });
 
       this.peer.on('open', (id) => {
         console.log('PeerJS initialized with ID:', id);
